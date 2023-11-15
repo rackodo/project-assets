e949bda99f273f3852d714c5d4e146c78ea84a76
Bash Elliott <bashelliott@gmail.com>
2023-11-15 11:32:54 +1100

    add image load test

--- usf-clicker/scripts/modify.js
+++ usf-clicker/scripts/modify.js
@@ -1,4 +1,5 @@
 export function Modify(Game) {
+	// Update Info Page
 	fetch('https://assets.rackodo.dev/usf-clicker/text/modInfo.txt')
 		.then((response) => response.text().then(
 			function(text) {
@@ -6,4 +7,7 @@ export function Modify(Game) {
 			}
 		)
 	)
+
+	// Update Images
+	Game.Loader.Replace('perfectCookie.png', 'https://media.discordapp.net/attachments/1169961839816364042/1174109281407209592/littleguy-removebg-preview.png?ex=6566657a&is=6553f07a&hm=b31e3e1e57cf0d76d0ca1110fd9dd488e286d7bd1068dd2e3564163e08ef91dd&=')
 }
