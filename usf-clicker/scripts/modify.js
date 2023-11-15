52198598590301c79d2397c566013c65fb217803
Bash Elliott <bashelliott@gmail.com>
2023-11-15 12:36:17 +1100

    add shug

--- usf-clicker/scripts/modify.js
+++ usf-clicker/scripts/modify.js
@@ -9,7 +9,9 @@ export function Modify(Game) {
 	)
 
 	// Update Images
-	Game.Loader.Replace('perfectCookie.png', 'https://media.discordapp.net/attachments/1169961839816364042/1174109281407209592/littleguy-removebg-preview.png?ex=6566657a&is=6553f07a&hm=b31e3e1e57cf0d76d0ca1110fd9dd488e286d7bd1068dd2e3564163e08ef91dd&=')
+	Game.Loader.Replace('perfectCookie.png', 'https://assets.rackodo.dev/usf-clicker/images/perfectCookie.png')
+
+	Game.Loader.Replace('grandma.png', 'https://assets.rackodo.dev/usf-clicker/images/grandma.png')
 
 	// Replace Draw
 	Game.Draw=function()
