330a8a93be7026c182573342c45f28368675a557
Bash Elliott <bashelliott@gmail.com>
2023-11-16 23:39:57 +1100

    test

--- usf-clicker/scripts/USF.js
+++ usf-clicker/scripts/USF.js
@@ -991,9 +991,9 @@ AddLanguage('USF','US Furries',{
 	"[Cursor quote]Autoclicks once every 10 seconds.": "Autoclicks once every 10 seconds.",
 	"[Cursor business name]Rolling pin": "Rolling pin",
 	"[Cursor business quote]Essential in flattening dough. The first step in cookie-making.": "Essential in flattening dough. The first step in shugat-making.",
-	"Grandma": "Zeke",
-	"grandma": "zeke",
-	"grandmas": "zekes",
+	"Zeke": "/",
+	"zeke": "/",
+	"zekes": "/",
 	"%1 grandma": [
 		"%1 zeke",
 		"%1 zekes"
