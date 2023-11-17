4532659332fda0cc7da9c8ef230f8825a07966a5
Bash Elliott <bashelliott@gmail.com>
2023-11-17 12:27:34 +1100

    ?

--- usf-clicker/scripts/modify.js
+++ usf-clicker/scripts/modify.js
@@ -77,8 +77,8 @@ export function Modify(Game) {
 			if(Game.UpgradesById[i].buildingTie != 0) {
 				let test = "cookie clicker"
 				Game.UpgradesById[i]
-				test.replace("cookie", Game.UpgradesById[i].buildingTie.dname)
-				console.log(Game.UpgradesById[i].buildingTie.dname)
+				test.replace(/cookie/, Game.UpgradesById[i].buildingTie.dname)
+				console.log(test)
 			}
 		}
 	},"what the fuck")
